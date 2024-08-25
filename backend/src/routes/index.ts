546d03ed7d2a710296d26ee6f818354c2a662e5b
Alaa Eltayb <alaa.eltayb@hotmail.com>
2024-08-25 04:30:31 +0300

    added the wild card route handel  to ease the access to add hotel feature

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const app = express.Router();
 import userROutes from "./users.routes/index";
 import myHotelRoutes from "./myHotelRoutes.routes/index";
+import path from "path";
 
 app.use("/api/v1/user", userROutes);
 app.use("/api/v1/my-hotel-routes", myHotelRoutes);
-
+app.use("*", (req: Request, res: Response) => {
+  res.sendFile(
+    path.join(__dirname, ".../../../frontend/forntend/dist/index.html")
+  );
+});
 export default app;
