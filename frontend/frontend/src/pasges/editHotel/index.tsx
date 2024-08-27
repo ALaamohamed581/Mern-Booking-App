6b6a6bd65311596420e7082b69e2afb6a23c4d95
Alaa Eltayb <alaa.eltayb@hotmail.com>
2024-08-28 02:58:17 +0300

    test

--- frontend/frontend/src/pasges/editHotel/index.tsx
+++ frontend/frontend/src/pasges/editHotel/index.tsx
@@ -1,34 +1,3 @@
-// import { useMutation, useQuery } from "react-query";
-// import { useParams } from "react-router-dom";
-// import * as apiClient from "../../apis/api-clients";
-// import MangeHotelForm from "../../forms/MangeHotelForm";
-// const EditHotel = () => {
-//   const { hotelId } = useParams();
-//   const { data: hotel } = useQuery(
-//     "fetMyHotelById",
-//     () => apiClient.fetchMyHotelById(hotelId || ""),
-//     {
-//       enabled: !!hotelId,
-//     }
-//   );
-//   const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
-//     onSuccess: () => {},
-//     onError: () => {},
-//   });
-//   const handelSave = (hotelFormData: FormData, id) => {
-//     mutate(hotelFormData, id);
-//   };
-//   return (
-//     <MangeHotelForm
-//       hotel={hotel!}
-//       onSave={handelSave}
-//       id={hotelId}
-//       isLoading={isLoading}
-//     />
-//   );
-// };
-
-// export default EditHotel;
 import { useMutation, useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import * as apiClient from "../../apis/api-clients";
@@ -37,12 +6,8 @@ import { UseAppContext } from "../../context/AppContext";
 
 const EditHotel = () => {
   const { hotelId } = useParams();
-  const { data: hotel } = useQuery(
-    "fetchMyHotelById",
-    () => apiClient.fetchMyHotelById(hotelId || ""),
-    {
-      enabled: !!hotelId,
-    }
+  const { data: hotel } = useQuery("fetchMyHotelById", () =>
+    apiClient.fetchMyHotelById(hotelId as string)
   );
   const { showToast } = UseAppContext();
   const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
