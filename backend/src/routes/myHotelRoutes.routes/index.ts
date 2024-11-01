c54a2867bcf2722e802e47b27e53cbd243dc3082
ALaa Eltayb <120136455+ALaamohamed581@users.noreply.github.com>
2024-11-01 09:40:24 +0200

    Update index.ts

--- backend/src/routes/myHotelRoutes.routes/index.ts
+++ backend/src/routes/myHotelRoutes.routes/index.ts
@@ -4,6 +4,6 @@ import hotelRoute from "./hotelRoute";
 import { verifyToken } from "../../middleware/verifyToken/token.validate.ts";
 
 const allowedUsers = ["buyer"];
-app.use(verifyToken , hotelRoute);
+app.use(hotelRoute);
 
 export default app;
